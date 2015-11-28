27ad09d3b5c40d292c3d3d93982bc419f047dedd
zhangyuanyuanol <1049960189@qq.com>
2015-11-28 18:01:00 +0800

    update

--- ajax.js
+++ ajax.js
@@ -1,87 +1,65 @@
 //版权 北京智能社©, 保留所有权利
 
-/*
-url, data, type, timeout, success, error
-*/
-
-function ajax(options)
-{
-	options=options||{};
-	options.data=options.data||{};
-	options.type=options.type||'get';
-	options.timeout=options.timeout||0;
-	
-	
-	options.data.t=Math.random();
-	
-	//data->'sss&ddd&xxx'
-	var arr=[];
-	for(var i in options.data)
-	{
-		//arr.push(i+'='+data[i]);
-		arr.push(i+'='+encodeURIComponent(options.data[i]));
+function json2url(json){
+	var arr = [];
+	json.t = Math.random();
+	for(var name in json){
+		arr.push(name + "=" + json[name]);
 	}
-	var str=arr.join('&');
-	
-	//1.创建
-	if(window.XMLHttpRequest)
-	{
-		var oAjax=new XMLHttpRequest();
-	}
-	else
-	{
-		var oAjax=new ActiveXObject('Microsoft.XMLHTTP');
-	}
-	
-	//2.连接
-	//alert(url+'?'+str);
-	if(options.type=='get')
-	{
-		oAjax.open('get', options.url+'?'+str, true);
-		oAjax.send();
-	}
-	else
-	{
-		oAjax.open('post', options.url, true);
-		
-		oAjax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-		//oAjax.setRequestHeader('名字', '值');
-		
-		oAjax.send(str);
-	}
-	
-	//4.接收
-	oAjax.onreadystatechange=function ()
-	{
-		if(oAjax.readyState==4)
-		{
-			if(oAjax.status>=200 && oAjax.status<300 || oAjax.status==304)
-			{
-				options.success && options.success(oAjax.responseText);
-			}
-			else
-			{
-				options.error && options.error();
-			}
-		}
-	};
-	
-	if(options.timeout)
-	{
-		//超时
-		setTimeout(function (){
-			oAjax.abort();
-		}, options.timeout);
-	}
+	return arr.join("&");
 }
 
-
-
-
-
-
-
-
-
-
-
+//options url,data,type,success,error,timeout
+function ajax(options){
+	
+	options = options || {};
+	options.data = options.data || {};
+	options.type = options.type || "get";
+	options.timeout = options.timeout || 0;
+		
+	if(!options.url){
+		return ;
+	}
+	
+	var str = json2url(options.data);
+	
+	//1 创建对象
+	if(window.XMLHttpRequest){
+		var xhr = new XMLHttpRequest();
+	} else {
+		var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	} 
+	
+	if(options.type == "get"){
+		xhr.open("get",options.url + "?" + str,true);
+		xhr.send();
+	} else {
+		xhr.open("post",options.url,true);
+		
+		xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		 
+		xhr.send(str);	
+	}
+	
+	//4 接收
+	xhr.onreadystatechange = function(){
+		
+		if(xhr.readyState == 4){//完成  并不代表成功
+			
+			//http状态  2xx  304
+			clearTimeout(timer);
+			if(xhr.status >= 200 && xhr.status < 300 || xhr.status == 304){ 
+				//xhr.responseText
+				options.success && options.success(xhr.responseText); 
+			} else {
+				options.error && options.error();
+			}
+		}	
+	}; 
+	
+	if(options.timeout){
+		var timer = setTimeout(function(){
+			xhr.abort();
+		},options.timeout);
+	}
+}
